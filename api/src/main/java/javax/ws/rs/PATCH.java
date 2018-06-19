7c4f52c752460c2459aaa9ed4bb5f60e926f9e43
Jason Dillon <jdillon@users.noreply.github.com>
2018-06-19 14:22:15 -0700

    fix note

--- api/src/main/java/javax/ws/rs/PATCH.java
+++ api/src/main/java/javax/ws/rs/PATCH.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// HACK: JAX-RS 2.0 does not support PATH, 2.1 is supposed to though;
+// HACK: JAX-RS 2.0 does not support PATCH, 2.1 is supposed to though;
 // HACK: dropwizard-jersey has a compatible annotation io.dropwizard.jersey.PATCH, but we do not want to pull all that in here
 
 @Target({ElementType.METHOD})
